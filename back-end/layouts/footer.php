562562741945f5f0fbc8e011c411f80a15adc3da
Leonardo Duarte <95362445+leo-md@users.noreply.github.com>
2023-11-15 19:44:00 -0300

    Add files via upload

---
+++ back-end/layouts/footer.php
@@ -0,0 +1,4 @@
+<footer class='footer navbar-fixed-bottom'>
+    <p>SUPORTE TECNICO</P>
+    <P>0800-800-8000</P>
+</footer>
---
+++ back-end/layouts/menu.php
@@ -0,0 +1,8 @@
+<nav class="menuAdmin">
+    <a href="home.php">HOME</a>
+    <a href="clientes.php">PAINEL CLIENTES</a>
+    <a href="home.php">PAINEL FORNECEDORES</a>
+    <a href="home.php">RELATORIOS</a>
+    <a href="home.php">SUPORTE</a>    
+    <a href="logout.php" onMouseOver="this.style.color='red'" onMouseOut="this.style.color='#194240'">SAIR</a>
+</nav>

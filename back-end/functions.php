eb711d62c51027d4c771e7171f176a5cf801157c
Leonardo Duarte <95362445+leo-md@users.noreply.github.com>
2024-05-18 20:37:31 -0300

    Update functions.php
    Adição das funções  buscarVeiculo() e buscarPeca()

--- back-end/functions.php
+++ back-end/functions.php
@@ -18,7 +18,120 @@ function buscar($connect, $tabela, $where = 1, $order=""){
     return $resultado;
 }
 
-/*Inserir novos containers no BD*/
+/*buscar veículos conforme CodCliente*/
+function buscarVeiculo($connect, $tabela, $codCliente) {
+    $queryVeiculo = "SELECT * FROM $tabela WHERE codCliente = '$codCliente'";
+    $buscaVeiculo = mysqli_query($connect, $queryVeiculo);
+    $resultado = mysqli_fetch_all($buscaVeiculo, MYSQLI_ASSOC);
+    $verifica = mysqli_num_rows($buscaVeiculo);
+
+    if(empty($verifica)){
+        echo 'Nenhum veículo cadastrado';
+    } else {
+        foreach ($resultado as $linha) {
+            foreach ($linha as $chave => $valor) {
+                if ($chave == 'placa'){
+                    echo '
+                    <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#'. $valor .'" aria-controls="offcanvasExample">
+                      ' . $valor . ' <i class="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                    <div class="offcanvas offcanvas-end" tabindex="-1" id="'. $valor .'" aria-labelledby="offcanvasExampleLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasExampleLabel">Placa: '. $valor .'</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <div>
+                            Marca: '. $linha['marca'].' <br>
+                            Modelo: '. $linha['modelo'].' <br>
+                            Fabricação / Modelo: '. $linha['anoFabrica'].' / '. $linha['anoModelo'] .' <br>
+                            Quilometragem: '. number_format($linha['quilometragem'],0,',','.'). ' <br>' . buscarPeca($connect, 'peca', $valor, $linha['quilometragem']) .                            
+                        '</div>
+                    </div>
+                    </div>';
+                }
+            }
+        }
+    }
+}
+
+/*buscar peças conforme CodPlaca*/
+function buscarPeca($connect, $tabela, $codPlaca, $kmVeiculo) {
+    $queryPeca = "SELECT * FROM $tabela WHERE codPlaca = '$codPlaca'";
+    $buscaPeca = mysqli_query($connect, $queryPeca);
+    $resultado = mysqli_fetch_all($buscaPeca, MYSQLI_ASSOC);
+    $verifica = mysqli_num_rows($buscaPeca);
+
+    $output = ''; // Variável para armazenar os resultados das peças
+
+    if(empty($verifica)){
+        // Se não houver peças, retornar uma string vazia
+    } else {
+        $output .= '<div class="offcanvas-body">'; // Iniciando o offcanvas-body para as peças
+        foreach ($resultado as $linha) {
+            $condicao = 'Erro de avaliação, verificar';
+            foreach ($linha as $chave => $valor) {
+                if ($chave == 'codPlaca'){
+                    $vida_util_restante = $linha['km_ultima_substituicao'] + $linha['vida_util_esperada'] - $kmVeiculo;
+                    $depreciacao = round($vida_util_restante / $linha['vida_util_esperada'] * 100);
+                    if ($depreciacao < 2){
+                        $depreciacao = 2;
+                    }
+                    $barraProgresso = '
+                                        <div class="progress">
+                                            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 138%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+                                        </div>';
+                    if ($vida_util_restante == $linha['vida_util_esperada']) {
+                        $condicao = 'Novo';
+                    } elseif ($vida_util_restante > 0.5 * $linha['vida_util_esperada']) {
+                        $condicao = 'Bom';
+                    } elseif ($vida_util_restante > 0.1 * $linha['vida_util_esperada'] && $vida_util_restante <= 0.5 * $linha['vida_util_esperada']) {
+                        $condicao = 'Revisar';
+                    } elseif ($vida_util_restante <= 0.1 * $linha['vida_util_esperada']) {
+                        $condicao = 'Desgastado';
+                    }
+                    if ($depreciacao == 100) {
+                        $barraProgresso = '
+                        <div class="progress">
+                            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: '. $depreciacao .'%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+                        </div>';
+                    } elseif ($depreciacao < 100 && $depreciacao >= 50) {
+                        $barraProgresso = '
+                        <div class="progress">
+                            <div class="progress-bar progress-bar-striped bg-success progress-bar-animated" role="progressbar" style="width: '. $depreciacao .'%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+                        </div>';
+                    } elseif ($depreciacao < 50 && $depreciacao > 10) {
+                        $barraProgresso = '
+                        <div class="progress">
+                            <div class="progress-bar progress-bar-striped bg-warning progress-bar-animated" role="progressbar" style="width: '. $depreciacao .'%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+                        </div>';
+                    } elseif ($depreciacao < 10) {
+                        $barraProgresso = '
+                        <div class="progress">
+                            <div class="progress-bar progress-bar-striped bg-danger progress-bar-animated" role="progressbar" style="width: '. $depreciacao .'%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+                        </div>';
+                    }
+
+                    if($linha['km_ultima_substituicao'] == 0){
+                        $instalacao = 'Original de Fábrica';
+                    } else {
+                        $instalacao = number_format($linha['km_ultima_substituicao'],0,',','.');
+                    }
+                    $output .= '
+                            <br>
+                            Peça: '. $linha['nome'].' <br>
+                            Vida útil esperada: '. number_format($linha['vida_util_esperada'],0,',','.').' <br>
+                            KM instalação: '. $instalacao .' <br>
+                            Estado de conservação: ' . $condicao.' <br>' . $barraProgresso;
+                }
+            }
+        }
+        $output .= '</div>'; // Fechando o offcanvas-body para as peças
+    }
+
+    return $output; // Retornando os resultados das peças
+}
+/*Inserir novos clientes no BD*/
 function inserirContainer($connect){
     if(isset($_POST['cadastrar']) AND !empty($_POST['cliente']) AND !empty($_POST['cic']) AND !empty($_POST['telefone'])){
         $erros = array();
